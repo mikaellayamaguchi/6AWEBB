fde580742a5c76538553985f795c8135c726f6c5
mikaellayamaguchi <mikaellayamaguchi23@gmail.com>
2025-11-30 17:17:12 +0800

    Activity 4: Directives

--- angular-routing-lab/src/app/directives/directives.ts
+++ angular-routing-lab/src/app/directives/directives.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-directives',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './directives.html',
   styleUrl: './directives.css',
 })
 export class Directives {
+  isStaticNoteVisible: boolean = false;
+  isNoteVisible: boolean = true;
+  isParagraphVisible: boolean = true;
 
+  showNote() {
+    this.isNoteVisible = true;
+  }
+
+  hideNote() {
+    this.isNoteVisible = false;
+  }
+
+  toggleNote() {
+    this.isParagraphVisible = !this.isParagraphVisible;
+  }
+
+  monthNameStatic: string = 'Mar';
+  monthNameDynamic: string = 'Mar';
+
+  cityList: string[] = ["Angeles", "San Fernando", "Mabalacat", "Tarlac", "Bataan"]
+
+  studentList: any[] = [
+    {stud_name: 'D Esquivel', course: 'Web Development', isActive:false},
+    {stud_name: 'J Dizon', course: 'Network Administration', isActive:false},
+    {stud_name: 'F Alejandro', course: 'Systems Development', isActive:false},
+    {stud_name: 'J David', course: 'CyberSecurity', isActive:false},
+    {stud_name: 'C Quintana', course: 'Web Development', isActive:true},
+  ]
 }
