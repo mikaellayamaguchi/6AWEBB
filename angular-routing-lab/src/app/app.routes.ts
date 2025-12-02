6b0fc47e78b72b66025ca67f5f3a3dab38f2c004
mikaellayamaguchi23 <mikaellayamaguchi23@gmail.com>
2025-12-02 09:09:37 +0800

    BOOTSTRAP & TAILWIND

--- angular-routing-lab/src/app/app.routes.ts
+++ angular-routing-lab/src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { Home } from './home/home';
 import { DataBinding } from './data-binding/data-binding';
 import { Directives } from './directives/directives';
 import { PageNotFound } from './page-not-found/page-not-found';
+import { Tailwind } from './tailwind/tailwind';
 
 export const routes: Routes = [
  { path: '', component: Home },
  { path: 'data-binding', component: DataBinding },
  { path: 'directives', component: Directives },
- { path: '**', component: PageNotFound }
+ { path: 'tailwind', component: Tailwind},
+ { path: '**', component: PageNotFound },
 ];
+
