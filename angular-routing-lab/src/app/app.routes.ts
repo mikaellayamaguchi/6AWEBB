435cd4ea1d13ff4d56c1af585471aff85711452d
mikaellayamaguchi23 <mikaellayamaguchi23@gmail.com>
2025-12-02 08:07:56 +0800

    BOOTSTRAP

--- angular-routing-lab/src/app/app.routes.ts
+++ angular-routing-lab/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { DataBinding } from './data-binding/data-binding';
 import { Directives } from './directives/directives';
+import { PageNotFound } from './page-not-found/page-not-found';
 
 export const routes: Routes = [
  { path: '', component: Home },
  { path: 'data-binding', component: DataBinding },
- { path: 'directives', component: Directives }
+ { path: 'directives', component: Directives },
+ { path: '**', component: PageNotFound }
 ];
