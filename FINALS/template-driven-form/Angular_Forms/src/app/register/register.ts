aaca2cda133f2b6168496396f713134330141b66
mikaellayamaguchi <mikaellayamaguchi23@gmail.com>
2026-02-28 22:35:16 +0800

    act lab 4: register

---
+++ FINALS/template-driven-form/Angular_Forms/src/app/register/register.ts
@@ -0,0 +1,89 @@
+import { DatePipe, CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+// Angular Material Dependencies
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSliderModule } from '@angular/material/slider';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [
+    MatIconModule,    
+    MatCardModule,  
+    MatToolbarModule,
+    CommonModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSliderModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  templateUrl: './register.html',
+  styleUrls: ['./register.css']
+})
+export class Register {
+  // Data Model
+  userName: string = '';
+  email: string = '';
+  password: string = '';
+  gender: string = '';
+  birthDate!: Date;
+  address: string = '';
+  angularSkillLevel: number = 5;
+  submitted = false;
+  minSkillLevel = 1;
+  maxSkillLevel = 10;
+
+  // Form Group Validation Setup
+  formdata: FormGroup = new FormGroup({
+    userName: new FormControl(''),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    gender: new FormControl('', [Validators.required]),
+    birthDate: new FormControl(null, [Validators.required]),
+    address: new FormControl(''),
+    angularSkillLevel: new FormControl(5)
+  });
+
+  // Submit Handler
+  onClickSubmit(data: {
+    userName: string;
+    email: string;
+    password: string;
+    gender: string;
+    address: string;
+    birthDate: Date;
+    angularSkillLevel: number;
+  }) {
+    this.submitted = true;
+    this.userName = data.userName;
+    this.email = data.email;
+    this.password = data.password;
+    this.gender = data.gender;
+    this.address = data.address;
+    this.angularSkillLevel = data.angularSkillLevel;
+    this.birthDate = data.birthDate;
+
+    if (this.formdata.valid) {
+      console.log("Form Submitted!", this.formdata.value);
+    } else {
+      console.log('Form is not valid!');
+    }
+  }
+}
